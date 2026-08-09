fb5deb216243487acba9ba25000b257eb87a6ddf
franssantander <santanderfrancisbeam@gmail.com>
2026-08-09 15:35:36 +0800

    refactor: update MarketingLayout component to use explicit type for children prop

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,6 +1,10 @@
 import { Navbar } from "@/features/landing/components/navbar";
 
-export default function MarketingLayout({ children }: LayoutProps<'/'>) {
+export default function MarketingLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <>
       <Navbar />
